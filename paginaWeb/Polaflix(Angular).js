3ac2c8d5d698f0f954c1213481837c982f4b29ff
AlvaroSainzBarcena <alvarosainzbarcenag3@gmail.com>
2018-08-28 19:02:12 +0200

    Mejoras en la web
    Mejoras en la interfaz AgregarSerie, y se consigue pasar datos entre routers

--- paginaWeb/Polaflix(Angular).js
+++ paginaWeb/Polaflix(Angular).js
@@ -10,7 +10,8 @@ polaflixApp.controller('userController', function ($scope, $http) {
 });
 
 
-polaflixApp.controller('muestraSeriesPrincipal', function ($scope, $http) {
+polaflixApp.controller('muestraSeriesPrincipal', function ($scope, $http, servicioVerSerie) {
+
   $http.get('http://localhost:8080/series').
     then(function (response) {
       $scope.seriesEmpezadas = response.data;
@@ -22,13 +23,45 @@ polaflixApp.controller('muestraSeriesPrincipal', function ($scope, $http) {
   $http.get('http://localhost:8080/usuarios/' + user + '/seriesTerminadas').
     then(function (response) {
       $scope.seriesTerminadas = response.data;
-    });
+    }); 
 
-    $scope.verSerieFun = function(serieSeleccionada) {
-      $scope.serieSelec=serieSeleccionada;
+    $scope.verSerieFun = function(serie) {
+
+      serieSelec=serie;
+
   };
 
-});
+  // Metodo que comprueba si se ha modificado la variable serieSelec. En tal caso, actualiza su valor para que lo lea
+  // el router VerSerie.html desde la variable de scope
+  $scope.$watch('serieSelec', function(){
+    $scope.serieSelec=serieSelec;
+  });
+
+});// fin controlador muestraSeriesPrincipal
+
+
+polaflixApp.controller('controllerAgregarSerie', function ($scope, $http) {
+
+  $scope.letras=['A','B','C'];
+
+  $http.get('http://localhost:8080/series').
+    then(function (response) {
+      $scope.series = response.data;
+    });
+
+    // funcion para mostrar las series con la inicial pasada por parametro
+    $scope.muestraSeriesInicial = function(inicial) {
+
+      $http.get('http://localhost:8080/series/inicial/'+inicial).
+      then(function (response) {
+        $scope.seriesInicial = response.data;
+      });
+
+  };
+  
+
+});//fin controllerAgregarSerie
+
 
 
 // ROUTING     
@@ -50,6 +83,16 @@ polaflixApp.config(function ($routeProvider) {
       templateUrl: "VerSerie.html"
     });
 });
+
+ 
+// Servicio que inicializa y guarda en memoria la serie elegida en la ventana principal del usuario para pasarla
+// al router verSerie
+polaflixApp.factory('servicioVerSerie', function () {
+  serieSelec=null;
+  return serieSelec;
+});
+
+
 
 //ire definiendo mas controladores... poner un controlador para el usuario y otro para las series, con eso es suficiente , y desde ahi accedo a todos sus elementos
 //polaflixApp.controller(){}
